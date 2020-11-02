2d92e787dca60e1f6009c39358210f46c8e6d58f
Eric Ranaldi <eric.ranaldi@gmail.com>
2020-11-01 18:49:47 -0700

    Add files via upload

---
+++ sneakysneaky/States/GuardStates.cs
@@ -0,0 +1,207 @@
+﻿/*
+ * Author: Eric Ranaldi
+ * Date: 2/20/2020
+ * 
+ * Purpose: To define all the states of the Guard and the game logic in each state
+ * 
+ * Credits: Full Sail instructors and staff who created NavAgentStateMachine_Best. I used that script to pull in the simple states such as idle and Pause. Also used the inspritation to create a GuardAgent getter function to use in the states.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace GuardStates
+{
+    public class GuardState : FSGDN.StateMachine.State
+    {
+        protected GuardAgent GuardAgentStateMachine()
+        {
+            return ((GuardAgent)machine);
+        }
+    }
+
+
+    public class IdleState : GuardState
+    {
+        float timer = 0;
+
+        public override void Enter()
+        {
+            base.Enter();
+            timer = 0;
+            int previousState = GuardAgentStateMachine().GetStateThatTriggeredIdle();
+            if(previousState != 0)
+            {
+                if(previousState == 1)
+                {
+                    GuardAgentStateMachine().SetMainColor(new Color(0.0f, 0.5f, 0.0f));
+                }
+                else if(previousState == 2)
+                {
+                    GuardAgentStateMachine().SetMainColor(new Color(0.5f, 0.0f, 0.0f));
+                }
+            }
+            else
+            {
+                Debug.LogError("Idle  state should not be called without setting triggerState");
+            }
+
+            GuardAgentStateMachine().ClearIdleTriggerState();
+           
+        }
+
+        public override void Execute()
+        {
+            timer += Time.deltaTime;
+            if (timer >= 2.0f)
+            {
+                if (!GuardAgentStateMachine().CheckGuardAlarmStatus())
+                {
+                    machine.ChangeState<PatrolState>();
+                    GuardAgentStateMachine().PickNextNavPoint();
+                }
+                else if(GuardAgentStateMachine().CheckGuardAlarmStatus())
+                {
+                    machine.ChangeState<AngryState>();
+                    GuardAgentStateMachine().PickNextNavPoint();
+                }
+            }
+        }
+    }
+
+    public class PatrolState : GuardState
+    {
+        public override void Enter()
+        {
+            base.Enter();
+
+            if (GuardAgentStateMachine().IsReversed() == true)
+            {               
+                GuardAgentStateMachine().SetMainColor(Color.blue);
+            }
+            else
+            { 
+                GuardAgentStateMachine().SetMainColor(Color.green);
+            }
+            GuardAgentStateMachine().SetCurrentTarget(null);
+            GuardAgentStateMachine().StartPatrolling();
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+
+            //Have the agent check for player in line of sight
+            bool foundPlayer = GuardAgentStateMachine().CheckForPlayer();
+            bool alarmTriggered = GuardAgentStateMachine().CheckGuardAlarmStatus();
+            if (foundPlayer || alarmTriggered)
+            {             
+                machine.ChangeState<AngryState>();
+            }
+        }       
+    }
+
+    public class PursueState : GuardState
+    {
+        public override void Enter()
+        {
+            base.Enter();
+            GuardAgentStateMachine().SetMainColor(Color.magenta);
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+
+            bool targetInRange = GuardAgentStateMachine().CheckDistanceToTarget();
+            bool invisCheck = GuardAgentStateMachine().CheckIfPlayerInvisble();
+            if (targetInRange && !invisCheck)
+            {
+                GuardAgentStateMachine().FollowCurrentTarget();
+            }
+            else
+            {
+                machine.ChangeState<PatrolState>();
+            }
+        }
+    }
+
+    public class AngryState : GuardState
+    {
+        private bool startPursueInEnter = false;
+        private bool travelingToAlarm = false;
+        public override void Enter()
+        {
+            base.Enter();
+            GuardAgentStateMachine().SetMainColor(Color.red);
+
+            bool isAlarmTriggeredAlready = GuardAgentStateMachine().CheckGuardAlarmStatus();
+            bool doesGuardHaveTarget = GuardAgentStateMachine().DoesGuardHaveTarget();
+
+            if (!isAlarmTriggeredAlready)
+            {
+                GuardAgentStateMachine().GoToAlarm();
+                GuardAgentStateMachine().SetTravelingToAlarm(true); //this could probably be combined into the GoToAlarm function since well it would be true if your telling the agent to go to alarm.
+            }
+            else if(doesGuardHaveTarget && isAlarmTriggeredAlready)
+            {
+                bool targetInRange = GuardAgentStateMachine().CheckDistanceToTarget();
+                if (targetInRange)
+                {
+                    machine.ChangeState<PursueState>();
+                    startPursueInEnter = true;
+                }
+                else
+                {
+                    GuardAgentStateMachine().StartPatrolling();
+                }
+             
+            }
+            else if(isAlarmTriggeredAlready)
+            {
+                GuardAgentStateMachine().StartPatrolling();
+            }                       
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+            if (!startPursueInEnter)
+            {
+                bool foundPlayer = GuardAgentStateMachine().CheckForPlayer();
+                bool checkAlarm = GuardAgentStateMachine().CheckGuardAlarmStatus();
+                travelingToAlarm = GuardAgentStateMachine().GetTravelingToAlarmStatus();
+
+                if (checkAlarm)
+                {
+                    if (foundPlayer)
+                    {
+                        if (!machine.IsCurrentState<PursueState>())
+                        {
+                            machine.ChangeState<PursueState>();
+                        }
+                    }
+                   
+                }
+                else if(!checkAlarm && !travelingToAlarm)
+                {
+                    Debug.LogWarning("Timer has expired. " + GuardAgentStateMachine().name + " going back to patrol");
+                    machine.ChangeState<PatrolState>();
+                }
+            }
+
+        }
+
+    }
+
+    public class PauseState : GuardState
+    {
+        public override void Enter()
+        {
+            base.Enter();
+            GuardAgentStateMachine().SetMainColor(Color.grey);
+        }
+    }
+}
---
+++ sneakysneaky/States/PlayerStates.cs
@@ -0,0 +1,198 @@
+﻿/*
+ * Author: Eric Ranaldi
+ * Date: 2/20/2020
+ * 
+ *Purpose: To define all the states of the Player and the game logic in each state
+ * 
+ * Credits: Full Sail instructors and staff who created NavAgentStateMachine_Best. I used that script to pull in the simple states such as
+ * idle state. Also used the inspritation to create a PlayerAgent getter function to use in the states.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using OMILGT.Utilities;
+
+namespace PlayerStates
+{
+    public class PlayerState : FSGDN.StateMachine.State
+    {
+        //Refrence to player Agent script
+        protected PlayerAgent GetPlayerAgentStateMachine()
+        {
+            return ((PlayerAgent)machine);
+        }
+    }
+
+    public class IdleState : PlayerState
+    {
+        public override void Enter()
+        {
+            base.Enter();
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+
+        }
+    }
+
+    public class GameOver : PlayerState
+    {
+        private float resetTimerLen = 5.0f;
+        private float timer = 0.0f;
+
+        public override void Enter()
+        {
+            base.Enter();
+
+            GameOverVal currentGameOverState = GetPlayerAgentStateMachine().GetGameOverFlag();
+
+            if (currentGameOverState == GameOverVal.YOUWIN)
+            {
+                GetPlayerAgentStateMachine().EnableGameOverUI("YOU ESCAPED!", Color.green);
+            }
+            else if (currentGameOverState == GameOverVal.YOULOSE)
+            {
+                GetPlayerAgentStateMachine().EnableGameOverUI("YOU HAVE BEEN CAUGHT!", Color.red);
+            }
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+            
+                timer += Time.deltaTime;
+                if (timer >= resetTimerLen)
+                {
+                    SceneManager.LoadScene(0);
+                }
+
+            
+        }
+    }
+
+
+    public class MoveState : PlayerState
+    {
+        public override void Enter()
+        {
+            base.Enter();
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+
+            Command currentCommand = GetPlayerAgentStateMachine().GetCurrentCommand();
+
+            if (currentCommand != null)
+            {
+                if (currentCommand.commandName == "MoveForward")
+                {
+                    currentCommand.Execute(GetPlayerAgentStateMachine().gameObject);
+                }
+                if (currentCommand.commandName == "MoveBack")
+                {
+                    currentCommand.Execute(GetPlayerAgentStateMachine().gameObject);
+                }
+                if (currentCommand.commandName == "MoveRight")
+                {
+                    currentCommand.Execute(GetPlayerAgentStateMachine().gameObject);
+                }
+                if (currentCommand.commandName == "MoveLeft")
+                {
+                    currentCommand.Execute(GetPlayerAgentStateMachine().gameObject);
+                }
+
+                if(currentCommand.commandName == "Stop")
+                {
+                    currentCommand.Execute(GetPlayerAgentStateMachine().gameObject);
+                }
+            }
+
+            GetPlayerAgentStateMachine().MovePlayer(Vector3.zero);
+            //machine.ChangeState<IdleState>();
+        }
+
+        public override void Exit()
+        {
+            base.Exit();
+        }
+    }
+
+    public class MouseMoveState : PlayerState
+    {
+        public override void Enter()
+        {
+            base.Enter();
+
+            GetPlayerAgentStateMachine().MovePlayer(Vector3.zero);
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+
+            //GetPlayerAgentStateMachine().InputHandler();
+
+            bool reachedDestination = GetPlayerAgentStateMachine().CheckIfAtDestination();
+            bool canPlayerMoveAgain = GetPlayerAgentStateMachine().GetMouseMoveAgainFlag();
+
+            if(canPlayerMoveAgain)
+            {
+                GetPlayerAgentStateMachine().MovePlayer(Vector3.zero);
+                GetPlayerAgentStateMachine().ResetMouseMoveFlag();
+            }
+            else if(reachedDestination)
+            {
+                machine.ChangeState<IdleState>();
+                Debug.LogWarning("Player moving to idle state after mouse move");
+            }
+
+        }
+    }
+
+
+    public class ActivateDashState : PlayerState
+    {
+        public override void Enter()
+        {
+            base.Enter();
+
+            GetPlayerAgentStateMachine().ClearNavPath();
+            GetPlayerAgentStateMachine().ActivateDash();
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+
+            if(GetPlayerAgentStateMachine().DashPointReached())
+            {
+                Debug.LogWarning("Dash Completed");
+                machine.ChangeState<MoveState>();
+            }
+        }
+    }
+
+    public class ActivateInvisState : PlayerState
+    {
+        public override void Enter()
+        {
+            base.Enter();
+            GetPlayerAgentStateMachine().ActivateInvisiblePower();
+            GetPlayerAgentStateMachine().SetPlayerBodyInvis();
+
+            //machine.ChangeState<IdleState>();
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+        }
+    }
+}
