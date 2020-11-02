4387b96632775b020a322a76aa3232b39282c1d0
Eric Ranaldi <eric.ranaldi@gmail.com>
2020-11-01 18:54:40 -0700

    Add files via upload

---
+++ sneakysneaky/API/OMILGT/Input/InputManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using OMILGT.Utilities;
+
+namespace OMILGT
+{
+   
+    namespace Input
+    {
+        public class InputManager : MonoBehaviour
+        {
+            // Start is called before the first frame update
+            void Start()
+            {
+
+            }
+
+            // Update is called once per frame
+            void Update()
+            {
+
+            }
+
+            public Command HandleInput()
+            {
+                return null;
+            }
+
+            
+        }
+    }
+}
