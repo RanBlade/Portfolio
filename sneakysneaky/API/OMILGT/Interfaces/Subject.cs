1a1c114c146e566816ef9dfdca0490de0b53f0f5
Eric Ranaldi <eric.ranaldi@gmail.com>
2020-11-01 18:54:13 -0700

    Add files via upload

---
+++ sneakysneaky/API/OMILGT/Interfaces/Subject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace OMILGT
+{
+    namespace Interfaces
+    {
+
+        public interface ISubject
+        {
+            void Notify();
+
+            void AddWatcher(IWatcher newWatcher);
+            void RemoveWatcher(IWatcher watcher);
+
+
+        }
+    }
+}
---
+++ sneakysneaky/API/OMILGT/Interfaces/Watcher.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OMILGT
+{
+    namespace Interfaces
+    {
+        public enum WatcherMessages
+        {
+            TIMERCOMPLETE,
+            TOTAL_MESSAGES
+        }
+
+        public interface IWatcher
+        {
+            void OnNotify(string watcherName, WatcherMessages message);
+        }
+    }
+}
